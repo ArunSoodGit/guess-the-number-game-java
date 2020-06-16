cd1700ee627514482b9790847beca5c6ec22bc39
Arun <arun.sood1998@gmail.com>
2020-06-16 21:40:27 +0200

    Initial commit

---
+++ src/statistics/NullStatistics.java
@@ -0,0 +1,21 @@
+package statistics;
+
+import players.Player;
+
+public class NullStatistics implements Statistics {
+
+    @Override
+    public void andTheWinnerIs(Player player) {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
